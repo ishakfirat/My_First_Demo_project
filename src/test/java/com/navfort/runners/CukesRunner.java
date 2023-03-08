2c43015b2b3c134f9080002c0f7126b8d44539f6
Ishak Firat <iishakfirat@gmail.com>
2023-03-08 21:20:59 +0000

    CukesRunner.java class created

---
+++ src/test/java/com/navfort/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.navfort.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin ={
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
+        features = "src/test/resources/features",
+        glue = "com/navfort/step_definitions",
+        dryRun = false,
+        tags = "@wip_ishak",
+        publish = true
+
+)
+
+public class CukesRunner {
+}
